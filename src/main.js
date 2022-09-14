8b84a987da6259595633e2129ece1e5eaab4e5a6
nynnerasch <nynne.andersen@yahoo.dk>
2022-09-14 14:02:52 +0200

    har gjort nogle ting og sådan

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 import "./style.css";
+import { animate, stagger, inView, scroll, timeline } from "motion";
+
+animate(
+  ".fadein",
+  { opacity: [1, 0.5, 0.7], scale: [1, 1.2, 1.3] },
+  { duration: 4, direction: "alternate", repeat: Infinity }
+);
+
+animate(
+  ".staggeranimation li",
+  { opacity: [0, 1] },
+  {
+    delay: stagger(1, { start: 2 }),
+  }
+);
